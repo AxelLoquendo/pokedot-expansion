68772076596f450a5c5d695e8cc573be2e3a9f4d
nerfis-code <nerfisalcantara2017@gmail.com>
2026-07-08 21:25:35 -0400

    🔨 Arreglar flujo de combate - Ahora se puede llevar un combate de inicio a fin

--- scenes/battlefield/battlefield.gd
+++ scenes/battlefield/battlefield.gd
@@ -10,3 +10,7 @@ func display_moveset(moveset: Array) -> void:
 	for i in range(moveset.size()):
 		var btn: Button = moveset_container.get_child(i)
 		btn.text = moveset[i]
+	moveset_container.show()
+
+func hide_options() -> void:
+	moveset_container.hide()
