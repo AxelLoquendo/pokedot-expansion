5856e1a340369759844a7062a8582f6cf98d5298
nerfis-code <nerfisalcantara2017@gmail.com>
2026-07-17 16:41:26 -0400

    🥷 Cambio en el procesamiento de stdin/stdout de la libreria @pkmn/sim PBattle -> PBattlePeer

--- scenes/battlefield/battlefield.gd
+++ scenes/battlefield/battlefield.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
-signal animation_completed(result: Variant)
+var is_animation_running := false
+var pbattlepeer = PBattlePeer.new()
+var packet: String
 
 @onready var moveset_container: GridContainer = $UI/ColorRect/TextureRect/GridContainer
 @onready var player_pokemon: Sprite2D = $Pikachu
@@ -12,15 +14,33 @@ signal animation_completed(result: Variant)
 
 
 func _ready() -> void:
-	var pbattle := PBattle.new()
-	add_child(pbattle)
+	pbattlepeer.start("nerfis", "Arcanine||Leftovers|Intimidate|Flareblitz,Extremespeed,Wildcharge,Morningsun|Impish|252,0,252,0,4,0||||||||")
+
+
+func _physics_process(delta: float) -> void:
+	if is_animation_running:
+		return
+
+	pbattlepeer.poll()
+	var ready_state = pbattlepeer.get_ready_state()
 	
-	$UI/ColorRect/TextureRect/GridContainer/TextureButton.connect("pressed", pbattle.write.bind("move 1"))
-	$UI/ColorRect/TextureRect/GridContainer/TextureButton2.connect("pressed", pbattle.write.bind("move 2"))
-	$UI/ColorRect/TextureRect/GridContainer/TextureButton3.connect("pressed", pbattle.write.bind("move 3"))
-	$UI/ColorRect/TextureRect/GridContainer/TextureButton4.connect("pressed", pbattle.write.bind("move 4"))
-	
-	$UI/ColorRect/TextureRect/GridContainer/TextureButton.grab_focus()
+	if ready_state == PBattlePeer.STATE_OPEN:
+		if pbattlepeer.get_available_packet_count():
+			packet = pbattlepeer.get_packet()
+			print(packet)
+			print(parse_battle_line(packet).args)
+			print(parse_battle_line(packet).kw_args)
+
+func present() -> void:
+	show()
+	$UI.show()
+
+
+func bind_choices(callback: Callable) -> void:
+	$UI/ColorRect/TextureRect/GridContainer/TextureButton.connect("pressed", callback.bind(">p1 move 1"))
+	$UI/ColorRect/TextureRect/GridContainer/TextureButton2.connect("pressed", callback.bind(">p1 move 2"))
+	$UI/ColorRect/TextureRect/GridContainer/TextureButton3.connect("pressed", callback.bind(">p1 move 3"))
+	$UI/ColorRect/TextureRect/GridContainer/TextureButton4.connect("pressed", callback.bind(">p1 move 4"))
 
 
 func display_moveset(moveset: Array) -> void:
@@ -31,6 +51,7 @@ func display_moveset(moveset: Array) -> void:
 
 func show_options() -> void:
 	moveset_container.show()
+	$UI/ColorRect/TextureRect/GridContainer/TextureButton.grab_focus()
 
 
 func hide_options() -> void:
@@ -90,3 +111,51 @@ func switch(ident: String, max_hp: int, hp: int) -> void:
 	player_hp_bar.set_value(hp)
 	label.text = pokemon
 	sprite2d.texture = load("res://graphics/pokemon/front/%s.png" % pokemon_id)
+
+
+class ParsedBattleLine:
+	var args: PackedStringArray = PackedStringArray()
+	var kw_args: Dictionary = {}
+
+
+func parse_battle_line(line: String) -> ParsedBattleLine:
+	var result := ParsedBattleLine.new()
+
+	# NOTA: Protocol.parseLine(line, true) del original hace un parseo especial
+	# para ciertos tipos de línea (chat, raw html, etc). Si tienes esa lógica
+	# en otro lado, iría aquí antes del fallback. La omito porque no fue provista.
+	if line == "|":
+		result.args.push_back("done")
+		return result
+
+	if line.is_empty() or line[0] != "|":
+		# línea inválida según el formato esperado por Showdown
+		return result
+
+	# args = line.slice(1).split('|')
+	var rest: String = line.substr(1)
+	var args: PackedStringArray = rest.split("|")
+
+	# Extraer kwArgs desde el final: mientras el último elemento
+	# tenga forma "[clave]valor"
+	while args.size() > 1:
+		var last_arg: String = args[args.size() - 1]
+		if last_arg.is_empty() or last_arg[0] != "[":
+			break
+
+		var bracket_pos: int = last_arg.find("]")
+		if bracket_pos <= 0:
+			break
+
+		var key: String = last_arg.substr(1, bracket_pos - 1)
+		var value_str: String = last_arg.substr(bracket_pos + 1).strip_edges()
+
+		if value_str.is_empty():
+			result.kw_args[key] = true  # Variant booleano, igual que en el original
+		else:
+			result.kw_args[key] = value_str
+
+		args.remove_at(args.size() - 1)
+
+	result.args = args
+	return result
--- scenes/game.gd
+++ scenes/game.gd
@@ -1,9 +1,12 @@
 extends Node
 
-@onready var p_server: PServer = $PServer
+#@onready var p_server: PServer = $PServer
+@export var battle_scene: Node
 
 func _ready() -> void:
 	pass
+	#p_battle = PBattle.new()
+	#battle_scene.add_child(p_battle)
 	# - No de incluye las versiones femeninas
 	# - Las variantes Gmax utiliza el sufijo _gmax en lugas de algun numero
 	# - Es muy muy probable que existan sprites de pokemon essentials
@@ -20,3 +23,9 @@ func _ready() -> void:
 	#var path := "res://graphics/pokemon/front/%s.png" % [species_id.to_upper() + n]
 	#var sprite: Texture2D = load(path)
 	#pokemon.texture = sprite
+
+# func _input(event: InputEvent) -> void:
+# 	if event.is_pressed() and event.as_text() == "B":
+# 		battle_scene.present()
+# 		p_battle.start("Jose", "Arcanine||Leftovers|Intimidate|Flareblitz,Extremespeed,Wildcharge,Morningsun|Impish|252,0,252,0,4,0||||||||")
+# 		get_viewport().set_input_as_handled()
